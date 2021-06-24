5d1aa14a70ec2f7cb3a96720a53d372263d77c2d
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-06-24 15:57:38 -0300

    Remove some lsp's
    html was buggy, so removed it too

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,16 +1,8 @@
 require('lspconfig').bashls.setup({})
 require('lspconfig').cssls.setup({})
-require('lspconfig').gopls.setup({})
-require('lspconfig').html.setup({})
+-- require('lspconfig').html.setup({})
 require('lspconfig').jsonls.setup({})
-require('lspconfig').pyls.setup({})
-require('lspconfig').rust_analyzer.setup({})
-require('lspconfig').solargraph.setup({})
-require('lspconfig').sqlls.setup({
-    cmd = { '/usr/local/bin/sql-language-server', 'up', '--method', 'stdio' },
-})
 require('lspconfig').tsserver.setup({})
 require('lspconfig').vimls.setup({})
 require('lspconfig').vuels.setup({})
 require'lspconfig'.phpactor.setup{}
-require'lspconfig'.tsserver.setup{}
