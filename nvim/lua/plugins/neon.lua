ac3551b1e9ffd121a807e1e9eff37c259fe5d4e0
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-07-16 09:53:23 -0300

    make test errors red with more readability on vim terminal

--- nvim/lua/plugins/neon.lua
+++ nvim/lua/plugins/neon.lua
@@ -9,3 +9,7 @@ vim.g.neon_italic_variable = true
 vim.g.neon_bold = true
 
 vim.cmd[[colorscheme neon]]
+local c = require("neon.colors")
+
+vim.g.terminal_color_1 = c.red
+vim.g.terminal_color_7 = c.bg0
