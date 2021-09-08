46201610df00913d70fdfc021e7126e231e180b6
Tiago Lisboa <tiago.caio.ol@gmail.com>
2021-09-08 09:35:16 -0300

    comment out lsp setup for phpactor and intelephense

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,6 +5,6 @@ require('lspconfig').jsonls.setup({})
 require('lspconfig').tsserver.setup({})
 require('lspconfig').vimls.setup({})
 require('lspconfig').vuels.setup({})
-require'lspconfig'.phpactor.setup{}
-require'lspconfig'.intelephense.setup{}
+# require'lspconfig'.phpactor.setup{}
+# require'lspconfig'.intelephense.setup{}
 require'lspconfig'.pyright.setup{}
