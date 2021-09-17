3eea08472af95458a20de008308695dc53e35b6a
Tiago Lisboa <tiago.lisboa@pitang.com>
2021-09-17 11:37:51 -0300

    breakpoints priority

--- nvim/lua/plugins/vimspector.lua
+++ nvim/lua/plugins/vimspector.lua
@@ -1,3 +1,11 @@
 local vim = vim
 
 vim.g.vimspector_enable_mappings = 'HUMAN'
+
+vim.g.vimspector_sign_priority = {
+    vimspectorPCBP=200,
+    vimspectorBP=200,
+    vimspectorBPCond=200,
+    vimspectorBPDisabled=9,
+    vimspectorCurrentThread=200
+}
